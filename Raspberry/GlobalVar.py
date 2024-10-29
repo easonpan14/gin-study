5f0bf60b41ee44aae5613025900da0919ec5c06d
tp6m4t <33980499+tp6m4t@users.noreply.github.com>
2024-10-29 16:02:20 +0800

    重構

---
+++ Raspberry/GlobalVar.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from openai import OpenAI
+
+class GlobalVar:
+    # Static variables
+    msg = ""
+    pwd = ""
+    gpt_id = 0
+    gpt_data = []
+    client = OpenAI(
+        api_key="sk-uJ3B62eXV4XouZSH7htWKYzf5QFj1W0WQd4AAn072WQPzptn",
+        base_url="https://api.chatanywhere.tech/v1"
+    )
+    date_str = datetime.now().strftime("%Y-%m-%d")
